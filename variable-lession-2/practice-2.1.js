8f510c3411069f413393e4118ae8ee6c3dd08f03
Roton02 <rph645102@gmail.com>
2025-03-12 23:20:31 +0600

    start the marhaba practice journey - 12-03-25

---
+++ variable-lession-2/practice-2.1.js
@@ -0,0 +1,34 @@
+//--------- problem-1
+//! store yout DOB in a variable
+
+const dateOfBirth = '2004-01-01'
+
+//--------- problem-2P
+//! store total number of your brother and sister  in a variable
+
+const brotherAndSisterCount = 5
+
+//--------- problem-3P
+//! store total number of your class student  in a variable
+
+const toatalStudentCount = 57
+
+//--------- problem-4P
+//! store total number of  last exam  in a variable
+
+const marks = 3.84
+
+//--------- problem-5P
+//! store total number of  room in your home  in a variable
+
+const totalRoom = 6
+
+//--------- problem-6P
+//! store total daily reading hours  in a variable
+
+const totalHours = 4
+
+//--------- problem-7P
+//! store total number of your brother and sister  in a variable
+
+const total = 27
