a4cd883263fda890c74fbbfabb14ce5dd4d83c24
Roton02 <rph645102@gmail.com>
2025-03-12 23:47:43 +0600

    add a new file for Practice lession 2 part 2 . solve 7 problems

---
+++ variable-lession-2/practice-2.2.js
@@ -0,0 +1,27 @@
+//--------- problem-1P
+const age = 22
+console.log(age)
+
+//--------- problem-2P
+const booksToRead = 5
+console.log(booksToRead)
+
+//--------- problem-3P
+wakeUpTime = 10
+console.log(wakeUpTime)
+
+//--------- problem-4P
+wastedTime = '8 hours 20 minutes'
+console.log(wastedTime)
+
+//--------- problem-5P
+const borrow = 6500
+console.log(borrow)
+
+//--------- problem-6P
+const goals = 4
+console.log(goals)
+
+//--------- problem-7P
+pillow = 2
+console.log(pillow)
