9d50851457b53cc946fdbb7ab0504b8185eba107
Roton02 <rph645102@gmail.com>
2025-03-13 00:01:06 +0600

    complete boolean problem lession 2 part 4

---
+++ variable-lession-2/practice-2.4.js
@@ -0,0 +1,28 @@
+//--------- problem-1P
+const isHappy = false
+console.log(isHappy)
+
+//--------- problem-2P
+const crushMessage = true
+console.log(crushMessage)
+
+//--------- problem-3P
+const celebreting = true
+console.log(celebreting)
+
+//--------- problem-4P
+const hasMoney = false
+const isHungry = false
+console.log(hasMoney, isHungry)
+
+//--------- problem-5P
+let isStudyDone = false
+console.log(isStudyDone)
+
+//--------- problem-6P
+let isPhoneCherged = false
+console.log(isPhoneCherged)
+
+//--------- problem-7P
+let isDinnerDone = true
+console.log(isDinnerDone)
