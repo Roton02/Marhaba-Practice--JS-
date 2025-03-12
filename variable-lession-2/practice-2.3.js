7208c79faa44715f1f7c553e1ee127e44c79a3b7
Roton02 <rph645102@gmail.com>
2025-03-12 23:54:57 +0600

    add a new file for Practice lession 2 part 23. solve 7 problem for string

---
+++ variable-lession-2/practice-2.3.js
@@ -0,0 +1,28 @@
+//--------- problem-1P
+
+const name = 'Roton'
+console.log(name)
+
+//--------- problem-2P
+const address = 'subarnachar noakhali bangladesh'
+console.log(address)
+
+//--------- problem-3P
+const institute = 'shariatpur polytechnic institute'
+console.log(institute)
+
+//--------- problem-4P
+const fan = 'i have no'
+console.log(fan)
+
+//--------- problem-5P
+const movieTheYear = 'i dont see any movie'
+console.log(movieTheYear)
+
+//--------- problem-6P
+const hobby = 'hajj with family'
+console.log(hobby)
+
+//--------- problem-7P
+const color = 'pink'
+console.log(color)
