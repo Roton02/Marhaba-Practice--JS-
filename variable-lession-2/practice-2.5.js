2ab38f16a17073673e01c0cd81cd06ffea2aeb9d
Roton02 <rph645102@gmail.com>
2025-03-13 20:35:10 +0600

    complete lession 2 all practice problem and create new practice lession folder for lession 3 for data types practices

---
+++ variable-lession-2/practice-2.5.js
@@ -0,0 +1,75 @@
+//--------- problem-1P
+let a = 100
+let b = 40
+const hasMoney = a - b
+console.log(hasMoney)
+
+//--------- problem-2P
+let pencil = 10
+let perUse = 2
+const haveTime = pencil / perUse
+console.log(haveTime)
+
+//--------- problem-3P
+{
+  let hasMoney = 60
+  let perUse = 15
+  const result = hasMoney / perUse
+  console.log(result)
+}
+
+//--------- problem-4P
+{
+  let firstWord = 'hello'
+  let lastWord = 'world'
+  const result = firstWord + lastWord
+  console.log(result)
+}
+
+//--------- problem-5P
+{
+  let firstBey = 153
+  let secondBey = 261
+  const result = firstBey + secondBey
+  console.log(result)
+}
+
+//--------- problem-6P
+{
+  let haveMoney = 500
+  let perUse = 75
+  const timesOfUse = haveMoney / perUse
+  const lastHaveMoney = haveMoney % perUse
+  console.log(timesOfUse, lastHaveMoney)
+}
+
+//--------- problem-7P
+{
+  let number = 8
+  const result = number / 3
+  const lastHaveMoney = number % 3
+  console.log(result, lastHaveMoney)
+}
+
+//--------- problem-8P
+{
+  let number = 50
+  const result = number % 9
+  console.log(result)
+}
+
+//--------- problem-9P
+{
+  let first = 'bangla'
+  let second = 'desh'
+  console.log(first + second)
+}
+
+//--------- problem-10P
+{
+  let perDay = 4
+  let total = 480
+  const existDay = total / perDay
+  const existMonth = (total / 30) * 4
+  console.log(existDay, existMonth)
+}
