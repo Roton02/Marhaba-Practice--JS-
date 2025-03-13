c2ff11ef9b2a128206ef478f9f3a5d6f13126327
Roton02 <rph645102@gmail.com>
2025-03-13 21:00:21 +0600

    complete first data type practice

---
+++ data-type-lession-3/practice-3.1.js
@@ -0,0 +1,35 @@
+//--------- problem-1P
+{
+  let price = 555
+  console.log(typeof price)
+}
+
+//--------- problem-2P
+{
+  let learning = true
+  console.log(typeof learning)
+}
+
+//--------- problem-3P
+{
+  let megaCity = 'dhaka' 
+  console.log(typeof megaCity)
+}
+
+//--------- problem-4P
+{
+  let temperature = 102.5
+  console.log(typeof temperature)
+}
+
+//--------- problem-5P
+{
+  let isHangry = true
+  console.log(typeof isHangry)
+}
+
+//--------- problem-6P
+{
+  let isRich = true 
+  console.log(typeof isRich)
+}
