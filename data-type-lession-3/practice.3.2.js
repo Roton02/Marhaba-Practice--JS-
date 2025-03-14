d92bbcff4c2b7efbe2d0158d19c31461def591a4
Roton02 <rph645102@gmail.com>
2025-03-14 23:07:27 +0600

    complete practice lession 3 part  let

---
+++ data-type-lession-3/practice.3.2.js
@@ -0,0 +1,44 @@
+//--------- problem-1P
+{
+  let age = 22
+  console.log(age)
+  age = 23
+  console.log('final output', age)
+}
+
+//--------- problem-2P
+{
+  let pocketMoney = 500
+  pocketMoney = 150
+  console.log('final output', pocketMoney)
+}
+//--------- problem-3P
+{
+  let studyTime = 4
+  studyTime = 0
+  console.log('final output', studyTime)
+}
+//--------- problem-4P
+{
+  let friendsCount = 3
+  friendsCount = 2
+  console.log('final output', friendsCount)
+}
+//--------- problem-5P
+{
+  let phonePrice = 25000
+  phonePrice = 12000
+  console.log('final output', phonePrice)
+}
+//--------- problem-6P
+{
+  let chairsCount = 40
+  chairsCount = 40 - 6
+  console.log('finalOutput', chairsCount)
+}
+//--------- problem-7P
+{
+  let playTime = 3
+  playTime -= 3
+  console.log('final output', playTime)
+}
