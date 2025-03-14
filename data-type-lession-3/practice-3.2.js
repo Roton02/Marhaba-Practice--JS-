e16b97a7fa26a097c3b138356936506082efb503
Roton02 <rph645102@gmail.com>
2025-03-14 23:20:43 +0600

    explore and practice  javascript comment

---
+++ data-type-lession-3/practice-3.2.js
@@ -0,0 +1,61 @@
+//--------- problem-1P
+{
+  // show the my last semester CGPA
+  let score = 3.84
+  console.log(score)
+}
+
+//--------- problem-2P
+{
+  // calculate and show the area of a circle with radius 5
+  let radius = 2
+}
+
+//--------- problem-3P
+{
+  /*
+    here it my liked 6 dress . 
+  */
+  let dress = 6
+  console.log(dress)
+}
+
+//--------- problem-4P
+{
+  // this gift from my young brother
+  let giftMoney = 100
+}
+
+//--------- problem-5P
+{
+  /*
+  1- first i check  my phone 
+  2- then i check my laptop
+  3- and finally i check my computer
+  4- and finally i check
+  5- and finally i check my book
+  */
+  let studyTime = 7
+}
+
+//--------- problem-6P
+{
+  /*
+  this for sendMoney 
+  sender need to 100
+  the fee is 5 tk 
+  */
+  let money = 100
+  console.log(money)
+}
+
+//--------- problem-7P
+{
+  /* 
+  this is for buying product the product is my shirt and pant 
+  the price of shirt is 800 and pant is 500
+  discount is 300 
+  */
+  let pay = 1000
+  console.log(pay)
+}
