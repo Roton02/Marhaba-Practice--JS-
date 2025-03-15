3d0639b857e55a31c3fd0c92944afafebb81d10d
Roton02 <rph645102@gmail.com>
2025-03-15 20:28:07 +0600

    learn about parsInt and ParFloat

---
+++ data-type-lession-3/practice-3.5.js
@@ -0,0 +1,43 @@
+//--------- problem-1P
+
+{
+  const taka = '20'
+  const output = parseInt(taka) + 10
+  console.log(output)
+}
+
+//--------- problem-2P
+{
+  const number = '3.15149'
+  console.log(parseFloat(number))
+}
+
+//--------- problem-3P
+{
+  const text = 'premikBoy'
+  console.log(parseInt(text)) //NaN
+}
+
+//--------- problem-4P
+{
+  const number = 3.1212 + 2.34232
+  console.log(number.toFixed(2))
+}
+//--------- problem-5P
+{
+  const number1 = 3.45342
+  const number2 = 2.34232
+  console.log(parseFloat(number1) + parseFloat(number2))
+}
+
+//--------- problem-6P
+{
+  const num1 = 4.232
+  const num2 = 3.2323
+  console.log((num1 + num2).toFixed(1))
+}
+
+//--------- problem-7P
+{
+  console.log((4.1234532).toFixed(3))
+}
