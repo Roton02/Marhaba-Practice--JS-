225bd25c2ff626876ed156b082c580f45910627d
Roton02 <rph645102@gmail.com>
2025-03-15 21:44:16 +0600

    explore javascript comparism oparator and also pracrtice this

---
+++ comaparism-lession-4/practice-4.1.js
@@ -0,0 +1,78 @@
+//--------- problem-1P
+
+{
+  const mySanduice = 7
+  const broSanduice = 3
+  if (mySanduice > broSanduice) {
+    console.log('ami besi khaisi ')
+  } else {
+    console.log('bro besi khaisi ')
+  }
+}
+
+//--------- problem-2P
+{
+  const myClass = 40
+  const otherClass = 50
+  if (myClass > otherClass) {
+    console.log('amr class boro ')
+  } else {
+    console.log('other class boro ')
+  }
+}
+
+//--------- problem-3P
+{
+  const myNumber = 75
+  const frndNumber = 75
+  const isSameNumber = myNumber === frndNumber
+  if (isSameNumber) {
+    console.log('same number ')
+  } else {
+    console.log('number is ot same ')
+  }
+}
+
+//--------- problem-4P
+{
+  const jomaRequired = 100
+  const joma = 95
+  if (jomaRequired > joma) {
+    const dorkar = jomaRequired - joma
+    console.log('aro dorkar akhono akso joi nai ', dorkar)
+  } else if (jomaRequired < joma) {
+    console.log('joma tk 100 er besi ', joma)
+  } else if (jomaRequired === joma) {
+    console.log('resuired joma and joma tk soman soman ')
+  }
+}
+
+//--------- problem-5P
+{
+  const firstNum = 10
+  const secondNum = 7
+  const isSoman = firstNum !== secondNum
+  if (isSoman) {
+    console.log(' soman na ')
+  } else {
+    console.log('same soman ')
+  }
+}
+
+//--------- problem-6P
+{
+  const check = 20 <= 15
+  console.log(check)
+}
+
+//--------- problem-7P
+{
+  const myNum = 45
+  const frndNum = 95
+  const check = myNum > frndNum
+  if (check) {
+    console.log('my number is greater ')
+  } else {
+    console.log('grnd number is greater ')
+  }
+}
---
+++ comaparism-lession-4/practice-4.2.js
@@ -0,0 +1,78 @@
+//--------- problem-1P
+{
+  const broAge = 10
+  const myAge = 15
+  if (myAge >= broAge) {
+    console.log('ami boro nahoi soman')
+  }
+}
+
+//--------- problem-2P
+{
+  const myNum = 45
+  if (myNum >= 33) {
+    console.log('passed')
+  }
+}
+
+//--------- problem-3P
+{
+  const jambura = 30
+  const cotbel = 20
+  if (jambura < cotbel) {
+    console.log('cotbel er dam besi')
+  } else if (jambura > cotbel) {
+    console.log('jambura er dam besi')
+  }
+}
+
+//--------- problem-4P
+{
+  const IRead = 10
+  const HeRead = 10
+  if (IRead === HeRead) {
+    console.log('I and he read same')
+  }
+}
+
+//--------- problem-5P
+{
+  const myBisccuit = 15
+  const frndBiscuit = 20
+  if (myBisccuit < frndBiscuit) {
+    console.log('ami kom biscuit paisi')
+  }
+}
+
+//--------- problem-6P
+{
+  const madrid = 2
+  const barsa = 3
+  if (madrid < barsa) {
+    console.log('barsa goal besi dise')
+  } else if (madrid > barsa) {
+    console.log('madrid besi dise')
+  }
+}
+
+//--------- problem-7P
+{
+  const myName = 'Rahim'
+  const frndName = 'Karim'
+  if (myName == frndName) {
+    console.log('same')
+  } else {
+    console.log('not same ')
+  }
+}
+
+//--------- problem-8P
+{
+  const IRead = true
+  const HeRead = false
+  if (IRead === HeRead) {
+    console.log('same')
+  } else {
+    console.log('not same')
+  }
+}
