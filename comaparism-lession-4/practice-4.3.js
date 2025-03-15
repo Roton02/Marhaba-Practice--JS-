78003066311f4b869b52d68037f6e54b55e9facf
Roton02 <rph645102@gmail.com>
2025-03-15 22:39:58 +0600

    complete double equals and tripple equals

---
+++ comaparism-lession-4/practice-4.3.js
@@ -0,0 +1,19 @@
+//--------- problem-1P
+{
+  const IHave = 50
+  const frndHave = '50'
+  console.log(IHave == frndHave)
+  console.log(IHave === frndHave)
+}
+//--------- problem-2P
+{
+  const notebook = 'JavaScript'
+  console.log(notebook == 'JavaScript')
+  console.log(notebook === 'JavaScript')
+}
+
+//--------- problem-3P
+{
+  console.log(25 == '25')
+  console.log(25 === '25')
+}
