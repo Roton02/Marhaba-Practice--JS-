b07195f0418b2693063697c91c5528a1505bb142
Roton02 <rph645102@gmail.com>
2025-03-15 20:12:23 +0600

    explore how to declare variable name in javascript

---
+++ data-type-lession-3/practice-3.4.js
@@ -0,0 +1,41 @@
+//--------- problem-1P
+{
+  let camelCase = 'amrPriyoKhabarHoloKacci'
+}
+
+//--------- problem-2P
+{
+  const city = 'Dhaka'
+  const City = 'Chattogram'
+  console.log(city, City)
+}
+//--------- problem-3P
+{
+  const username = 'Jovan'
+  const userName = 'Rivan'
+  console.log(userName, userName)
+}
+
+//--------- problem-4P
+{
+  const amarBasarThikana = 'Noakhalis'
+  console.log(amarBasarThikana)
+}
+
+//--------- problem-5P
+{
+  // const super = 30  //! Unexpected token 'super'
+  // console.log(super)
+}
+
+//--------- problem-6P
+{
+  // const class = 'diploma' //! Unexpected token 'class'
+  // console.log(class)
+}
+
+//--------- problem-7P
+{
+  // const 2ndPosition = 'sakib al hasan ' //!Unexpected token 'super'
+  // console.log(2ndPosition)
+}
