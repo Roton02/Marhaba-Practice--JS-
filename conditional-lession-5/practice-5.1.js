bfadfdf57dd1411ae7cf7a1899a1acdf5dcc8fb4
Roton02 <rph645102@gmail.com>
2025-03-16 17:58:29 +0600

    practice conditianal

---
+++ conditional-lession-5/practice-5.1.js
@@ -0,0 +1,59 @@
+// --------- problem -1
+{
+  const getIn = 5
+  if (getIn >= 6) {
+    console.log('nasta ready khete ay ')
+  } else {
+    console.log('jaru ready mair khete ay ')
+  }
+}
+
+// --------- problem -2
+{
+  let isLogin = true
+  if (isLogin) {
+    console.log('welcome babes')
+  } else {
+    console.log('dure giya mor babes')
+  }
+}
+
+//----------problem -3
+{
+  let running = 1
+  if (running <= 5) {
+    console.log('cocklet khete dibo babse')
+  } else {
+    console.log('khete na dibo babse')
+  }
+}
+
+//----------problem -4
+{
+  const number = 85
+  if (number > 80) {
+    console.log('bike kine dibo ')
+  } else {
+    console.log('dibo na sopno dekho')
+  }
+}
+
+//----------problem -5
+{
+  let showTime = 9
+  if (showTime == 9) {
+    console.log('movie dekhbo')
+  } else {
+    console.log('dekha lagbe guma ')
+  }
+}
+
+//----------problem -6
+{
+  let temperature = 30
+  if (temperature >= 30) {
+    console.log('earcondition cala')
+  } else {
+    console.log('kombol muri diye guma')
+  }
+}
