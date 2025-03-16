6563756b1bce3ca9408cdc796147e72b9b91d510
Roton02 <rph645102@gmail.com>
2025-03-16 17:42:44 +0600

    complete comparism oparator practice lessin 4

--- comaparism-lession-4/practice-4.3.js
+++ comaparism-lession-4/practice-4.3.js
@@ -17,3 +17,30 @@
   console.log(25 == '25')
   console.log(25 === '25')
 }
+//----------problem -4p
+{
+  console.log('apple' == 'apple')
+  console.log('apple' === 'apple')
+}
+
+//----------problem-5p
+{
+  console.log('test' == 'TEST')
+  console.log('test' === 'TEST')
+}
+
+//---------problem-6
+{
+  let variableX = 15
+  let variableY = '20'
+  console.log(variableX != variableY)
+  console.log(variableX !== variableY)
+}
+
+//---------problem-7
+{
+  let variableA = 'hello'
+  let variableB = 'Hello'
+  console.log(variableA == variableB)
+  console.log(variableA === variableB)
+}
